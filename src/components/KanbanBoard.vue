5957e819fa3fa4130d70f5f603772dc2e34a8384
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-11-25 19:11:25 +0100

    added hard coded uuid's to tasks and changed task.id type

--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -4,7 +4,7 @@ import TaskColumn from './TaskColumn.vue';
 import AddTask from "@/components/AddTask.vue";
 
 interface Task {
-  id: number | string; // string for UUID, number for hardcoded IDs
+  id: string; // string for UUID
   title: string;
   description: string;
 }
@@ -22,17 +22,17 @@ const columns = ref<Column[]>([
     title: 'To Do',
     tasks: [
       {
-        id: 1,
+        id: "47470ff2-9bad-4da1-b59d-a7c565a04106",
         title: 'Task 1',
         description: 'Description for Task 1'
       },
       {
-        id: 2,
+        id: "804a3a1b-99ef-479c-b3df-e9dccd923ba1",
         title: 'Task 2',
         description: 'Description for Task 2'
       },
       {
-        id: 3,
+        id: "85655854-0fe6-4cbf-aa33-12d82755f372",
         title: 'Task 3',
         description: 'Description for Task 3'
       }
@@ -43,7 +43,7 @@ const columns = ref<Column[]>([
     title: 'In Progress',
     tasks: [
       {
-        id: 4,
+        id: "06fc840e-6815-4006-8d23-24756d804fc8",
         title: 'Task 4',
         description: 'Description for Task 4'
       }
@@ -54,12 +54,12 @@ const columns = ref<Column[]>([
     title: 'Done',
     tasks: [
       {
-        id: 5,
+        id: "b7531d61-b1e6-4099-a2bf-821824831cb5",
         title: 'Task 5',
         description: 'Description for Task 5'
       },
       {
-        id: 6,
+        id: "8f817ddf-b013-4192-af3b-083473c7cc26",
         title: 'Task 6',
         description: 'Description for Task 6'
       }
